9f303c30b25c45be77406bfa262113caf1a5b9b9
Juyoung Moon <felixmoon@cenacle.com>
2025-01-10 02:43:59 +0900

    fix: API folder error

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  exportPathMap: async function (defaultPathMap) {
+    const pathMap = { ...defaultPathMap };
+    delete pathMap["/api/file"]; // API 라우트를 제거
+    return pathMap;
+  },
+};
 
 export default nextConfig;
